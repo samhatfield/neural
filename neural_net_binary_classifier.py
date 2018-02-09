8c41ae47596e9d124303a37dbcd8f74cda9bbaea
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2018-02-09 00:05:14 +0000

    First working version of binary classifier

--- neural_net_binary_classifier.py
+++ neural_net_binary_classifier.py
@@ -1,6 +1,6 @@
-from matplotlib.pyplot import plot, style, show, contour, subplots, clabel, scatter
+from matplotlib.pyplot import plot, style, show, contour, subplots, clabel, scatter, figure
 from numpy.random import rand
-from numpy import array, dot, exp, meshgrid, linspace, dstack, zeros, log, append, ones
+from numpy import array, dot, exp, meshgrid, linspace, dstack, zeros, log, append, ones, newaxis, sum, concatenate
 
 style.use('ggplot')
 
@@ -11,14 +11,10 @@ ax.set_ylim([0, 10])
 def plot_data(pairs):
     markers = ['s', '*']
 
-    print(pairs[:num_a,0])
-    print(pairs[:num_a,1])
-    print(pairs[num_a:,0])
-    print(pairs[num_a:,1])
     scatter(pairs[:num_a,0], pairs[:num_a,1], marker='s', c='black')
     scatter(pairs[num_a:,0], pairs[num_a:,1], marker='*', c='black')
 
-def plot_output(weights):
+def plot_contour(weights):
     domain = linspace(0, 10, 10)
     X, Y = meshgrid(domain, domain)
     Z = zeros((10,10))
@@ -34,33 +30,18 @@ def output(inputs, weights):
     return 1/(1+exp(-dot(inputs, weights[1:]) - weights[0]))
 
 def error_function(weights):
-    sum(t*log(output(x, weights)) + (1 - t)*log(1-output(x, weights)) for x, t in zip(inputs, labels))
-
-
-def update(weights):
-    # Compute output for each input
-    outputs = array([output(x, weights) for x in inputs])
-
-    # Compute error for each output
-    errors = outputs - labels
-
-    g = -[e * x for e, x in zip(errors, inputs)]
-    
-    # Compute cost function gradient
-    Δw = -η * sum(g)
-
-    # Increment weights
-    weights += Δw
-
-    return weights
-
+    return sum(t*log(output(x, weights)) + (1 - t)*log(1-output(x, weights)) for x, t in zip(inputs, labels))
 
 # Number of a and b points
 num_a = 10
 num_b = 10
 
+# Setup data storage
+freq = 100
+error_hist = []
+
 # Number of iterations for training
-n_iterations = 1
+n_iterations = 1000
 
 # Randomly generate training pairs for a
 inputs = 5*rand(num_a,2)
@@ -73,14 +54,32 @@ labels = append(labels, ones(num_b))
 # Plot data
 plot_data(inputs)
 
+# Weights initial condition
 weights = array([-0.5, 0.4, -0.2])
 
-plot_output(weights)
-#show()
+# Learning rate for gradient descent
+η = 0.05
 
 # Train
 for i in range(n_iterations):
-    weights = update(weights)
-    print(error_function(weights))
-    
+    if i%freq == 0:
+        error_hist.append(error_function(weights)) 
 
+    # Compute output for each input
+    outputs = array([output(x, weights) for x in inputs])
+
+    # Compute error for each output
+    errors = outputs - labels
+
+    g = errors[:,newaxis]*inputs
+    
+    # Compute cost function gradient
+    Δw = -η * concatenate(([sum(errors)], sum(g, axis=0)))
+
+    # Increment weights
+    weights += Δw
+
+plot_contour(weights)
+figure()
+plot(range(int(n_iterations/freq)), error_hist)
+show()
